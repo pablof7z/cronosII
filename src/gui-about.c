e0106c42df0db7d068c8211e14c8d96d7365576e
petergozz <petergozz>
2002-04-14 18:52:53 +0000

    text render problem fixed

--- src/gui-about.c
+++ src/gui-about.c
@@ -42,15 +42,10 @@ void gui_about_new (void) {
     "Yves Mettier <ymettier@libertysurf.fr>",
     "Peter Gossner <gossner@arcom.com.au>",
 	"Angel Ramos <seamus@debian.org>",
-    NULL
-  };
+    NULL };
   GtkWidget *href;
 
-  window = gnome_about_new ("CronosII", VERSION," Debian /GNU edition  
-		  		(C) GPL 2000-2002 Pablo Fernández Navarro",
-				authors,
-  				("A fast, flexible GNOME mail client."),
-			DATADIR "/cronosII/pixmap/splash.png");
+  window = gnome_about_new ("CronosII", VERSION, "Debian/GNU edition (C) GPL 2000-2002 Pablo Fernández Navarro", authors, _("A fast flexible GNOME Mail Client."), DATADIR "/cronosII/pixmap/splash.png");
   gtk_window_set_modal (GTK_WINDOW (window), TRUE);
   gtk_window_set_transient_for (GTK_WINDOW (window), GTK_WINDOW (WMain->window));
 
